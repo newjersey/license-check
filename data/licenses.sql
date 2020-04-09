60f2183fe9a95986fb23673fb66d6b489bf2dc51
Jordan Kasper <jordan.kasper@innovatenj.org>
2020-04-09 14:03:11 -0400

    Add volunteers SQL file for later use

--- data/licenses.sql
+++ data/licenses.sql
@@ -1,12 +1,12 @@
 CREATE TABLE njcovid.licenses
 (
+    id integer NOT NULL DEFAULT nextval('njcovid.licenses_id_seq'::regclass),
     license_number character varying(255) COLLATE "default" NOT NULL,
     first_name character varying(255) COLLATE "default" NOT NULL,
     last_name character varying(255) COLLATE "default" NOT NULL,
     state character varying(20) COLLATE "default" NOT NULL,
     status character varying(30) COLLATE "default" NOT NULL,
     date_of_birth character varying(10) COLLATE "default",
-    id integer NOT NULL DEFAULT nextval('njcovid.licenses_id_seq'::regclass),
     CONSTRAINT license_data_id PRIMARY KEY (id),
     CONSTRAINT license_data_number UNIQUE (license_number)
 );
